353fe9e6a03016984e1986b53c19346d4c365d04
Kinjal Modi <kvmodi1995@gmail.com>
2020-08-15 14:09:02 +0530

    added user field in notification

--- model/notification.js
+++ model/notification.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const notificationSchema = mongoose.Schema({
 	title: String,
 	description: String,
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
 	read: {
 		type: Boolean,
 		default: false
